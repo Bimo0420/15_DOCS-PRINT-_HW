212d0832be311be00359306d7230fec3e7386b74
Алексей Викулин <vaa@megamade.ru>
2022-10-26 12:30:24 +0300

    Добавьте файлы проекта.

---
+++ DOCS(PRINT)/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DOCS_PRINT_
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
