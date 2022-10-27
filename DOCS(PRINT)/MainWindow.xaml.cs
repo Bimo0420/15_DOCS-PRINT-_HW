e5a5e4dc2de5bd54ae92c71cf71ee70eb3e76c47
Алексей Викулин <vaa@megamade.ru>
2022-10-27 11:14:45 +0300

    Оказывается, при создании нового приложения я выбрала шаблон "Приложение WPF (NET Framework)" и даже не обратила на это внимания. Так вот при использовании NET Framework ваш код работает, а при использовании NETCore (шаблон приложения будет называться "Приложение WPF (Майкрософт)") - нет. Чтобы заработало в вашем случае, нужно для изображения установить следующие свойства

--- DOCS(PRINT)/MainWindow.xaml.cs
+++ DOCS(PRINT)/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -23,6 +25,23 @@ namespace DOCS_PRINT_
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using FileStream fs = File.Open("Метамодернизм.xaml", FileMode.Open);
+            docViewer.Document = XamlReader.Load(fs) as FlowDocument;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            using FileStream fs = File.Open("Метамодернизм.xaml", FileMode.Create);
+            XamlWriter.Save(docViewer.Document, fs);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            docViewer.ClearValue(FlowDocumentScrollViewer.DocumentProperty);
         }
     }
 }
